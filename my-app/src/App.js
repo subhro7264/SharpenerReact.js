be186924f4b680d1ed55ea26e006cb5721570d65
subhro7264 <subhrohazra7.com>
2023-08-06 16:27:28 +0530

    commit changes

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './component/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Lets get started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/component/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return(
+        <div><div>
+        <h>Expense Item!</h>
+    </div>
+    
+       <li>Food Rs 10</li>
+      <li>Petrol Rs 100</li>
+       <li>Movies Rs 200</li>
+   
+    </div>
+
+    )
+}
+ export default ExpenseItem;
