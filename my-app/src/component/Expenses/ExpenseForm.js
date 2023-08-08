d0f55a6974295c81a2472f222eba891b6ea5f924
subhro7264 <subhrohazra7.com>
2023-08-08 15:18:41 +0530

    git cheakout -b Submitting-the-forms

--- my-app/src/component/Expenses/ExpenseForm.js
+++ my-app/src/component/Expenses/ExpenseForm.js
@@ -4,18 +4,50 @@ const ExpenseForm = () => {
     const[enterTitle, setEnterTitle]=useState('');
     const[enterAmount, setEnterAmount]=useState('');
     const[enterDate, setEnterDate]=useState('');
+//     const [userInput,setUserInput]=useState({
+//  enterdTitle:'',
+//  enterdAmount:'',
+//    enterdDate:''
+// })
+
     const handleChangeTitle = (event) => {
     setEnterTitle(event.target.value);
+    
+//    setUserInput({ ...userInput, enterdTitle: event.target.value});
+// setUserInput((prevState)={
+//     return{
+// // ...prevState, enterdTitle: event.target.value
+// // }
+//     })
+
+
   };
 const handleChangeAmount=(event)=>{
     setEnterAmount(event.target.value);
+
+
+    // setUserInput({ ...userInput, enterdAmount: event.target.value});
 }
 const handleChangeDate=(event)=>{
     setEnterDate(event.target.value);
+
+//  setUserDate({ ...userInput, enterdAmount: event.target.value});
+
+}
+
+
+const submithandler=(e)=>{
+e.preventDefault();
+const expenseData={
+    title:enterTitle,
+    amount:enterAmount,
+    date: new Date(enterDate)
+}
+console.log(expenseData);
 }
   return (
     <div className="ExpenseForm ">
-      <form action="#">
+      <form onSubmit={submithandler} >
         <label >Expense_title</label>
         <input
           type="text" onChange={handleChangeTitle} placeholder=" Expense title"/>
@@ -25,7 +57,7 @@ const handleChangeDate=(event)=>{
         <br />
         <label>Expense_Date</label>
         <input type="date" onChange={handleChangeDate} placeholder=" Expense Date"/>
-        <button type="submit">
+        <button type="submit" >
           Submit
         </button>
       </form>
